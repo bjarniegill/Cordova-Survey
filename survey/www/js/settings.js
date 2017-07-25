19588dce8416efc4f98634e826abdac7b299bcbb
Bjarni Egill <bjarniegill@gmail.com>
2017-07-25 22:12:13 +0000

    add settings file

---
+++ survey/www/js/settings.js
@@ -0,0 +1,23 @@
+// ###########################
+// Time variables for schedule
+// ###########################
+const SURVEY_DURATION_IN_DAYS = 10;
+const SURVEYS_DONE_PER_DAY = 10;
+// in days
+const SURVEY_WAIT_PERIOD_BEFOR_START = 1;
+// 24 hour format(int)
+const SURVEY_START_HOUR = 10;
+const SURVEY_START_MINUTE = 0;
+const SURVEY_END_HOUR = 22;
+const SURVEY_END_MINUTE = 0;
+
+
+// ######################
+// Schedulig message text
+// ######################
+const SURVEY_SCHEDULE_TITLE_MESSAGE = "Daily Survey";
+const SURVEY_SCHEDULE_DISPLAY_MESSAGE = "Time for your next Dialy Survey!";
+
+// millisec
+// 4320000 = 72 min
+// 86400000 = 1 day
